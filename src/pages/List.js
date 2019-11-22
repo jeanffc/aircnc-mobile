d1575cf3b7b201b7a7620d630a8813c46330b824
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-11-22 17:01:14 -0300

    feat: added list page

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,10 +1,42 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  AsyncStorage,
+  View,
+  Text,
+  Image
+} from "react-native";
+
+import logo from "../assets/logo.png";
 
 export default function List() {
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("techs").then(storagedTechs => {
+      const techsArray = storagedTechs.split(",").map(tech => tech.trim());
+
+      setTechs(techsArray);
+
+      console.log(techsArray);
+    });
+  }, []);
   return (
-    <View>
-      <Text>List</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 10
+  }
+});
